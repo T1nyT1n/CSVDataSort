ad46ead231017d08b3d361926f60e31283df380f
T1nyT1n <120733478+T1nyT1n@users.noreply.github.com>
2026-06-21 23:32:55 +0300

    Chunks writing functionality.

--- external_sort.py
+++ external_sort.py
@@ -1,4 +1,29 @@
+import csv
+import sys
+
+MAX_CHUNK_SIZE = 0.01 * (1024 ** 3)
+
+def start():
+    with open('data.csv', 'r', newline='', encoding='utf-8') as data_file:
+        reader = csv.reader(data_file)
+        file_chunks_amount = 0
+        chunk = []
+        current_size = 0
+        for row in reader:
+            chunk.append(row)
+            current_size += sys.getsizeof(row)
+            if current_size >= MAX_CHUNK_SIZE:
+                with open(f'temp/chunk{file_chunks_amount}.csv', 'w', \
+                    newline='', encoding='utf-8') as chunk_file:
+                    writer = csv.writer(chunk_file)
+                    writer.writerows(chunk)
+                chunk = []
+                current_size = 0
+                file_chunks_amount += 1
+
 if __name__ == '__main__':
-    print("Заметьте, \033[33mэтот модуль не предназначен для самостоятельного" \
-        " запуска\033[0m. Используйте main.py для запуска программы. Запускаю" \
-        " main.py автоматически и завершаю работу...")
+    print('Заметьте, \033[33mэтот модуль не предназначен для самостоятельного' \
+        ' запуска\033[0m. Используйте main.py для запуска программы. Запускаю' \
+        ' main.py автоматически и завершаю работу...\n')
+    import runpy
+    runpy.run_module('main', run_name='__main__')
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import external_sort
 
 def main() -> None:
-    print("\033[1;31mCSVDataSort\033[0m by T1nyT1n.")
+    print('\033[1;31mCSVDataSort\033[0m by T1nyT1n.')
+    external_sort.start()
     
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
