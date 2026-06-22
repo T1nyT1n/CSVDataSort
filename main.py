de267c8f2710f2752870d5087b548428e4b010bc
Tiny Tin <120733478+T1nyT1n@users.noreply.github.com>
2026-06-22 12:47:50 +0300

    Improve welcome message.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import external_sort
 
 def main() -> None:
-    print('\033[1;31mCSVDataSort\033[0m by T1nyT1n.')
+    print('\033[31mCSVDataSort\033[0m by T1nyT1n.')
+    print('Посетите страницу с репозиторием на GitHub, чтобы узнать больше: ' \
+        '\033[33mhttps://github.com/T1nyT1n/CSVDataSort/\033[0m')
     external_sort.start()
     
 if __name__ == '__main__':
